1105c6b5f89e9af81ee4f3428a2b9646e90fa4a3
Mir Adnan Siraj <miradnansiraj@ymail.com>
2018-07-20 23:10:03 +0400

    fix BodyPartFragment to be subclass of Fragment, add headFragment to main activity

--- java/simple/app/called/androidme/ui/BodyPartFragment.java
+++ java/simple/app/called/androidme/ui/BodyPartFragment.java
@@ -1,17 +1,33 @@
 package simple.app.called.androidme.ui;
 
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
-import android.support.v7.app.AppCompatActivity;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
 
 import simple.app.called.androidme.R;
+import simple.app.called.androidme.data.AndroidImageAssets;
 
-public class BodyPartFragment extends AppCompatActivity
+public class BodyPartFragment extends Fragment
 {
-    @Override
-    protected void onCreate(@Nullable Bundle savedInstanceState)
+    private ImageView rootImage;
+    public BodyPartFragment()
     {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.fragment_body_part);
+
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState)
+    {
+        View rootView = inflater.inflate(R.layout.fragment_body_part, container, false);
+        rootImage = rootView.findViewById(R.id.body_part_image_view);
+        rootImage.setImageResource(AndroidImageAssets.getHeads().get(0));
+
+        return rootView;
     }
 }
--- java/simple/app/called/androidme/ui/MainActivity.java
+++ java/simple/app/called/androidme/ui/MainActivity.java
@@ -1,5 +1,6 @@
 package simple.app.called.androidme.ui;
 
+import android.support.v4.app.FragmentManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.Toast;
@@ -9,13 +10,16 @@ import simple.app.called.androidme.R;
 public class MainActivity extends AppCompatActivity
 {
     private static long back_pressed = 0;
-
+    private BodyPartFragment headFragment;
 
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        headFragment = new BodyPartFragment();
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        fragmentManager.beginTransaction().add(R.id.head_container, headFragment).commit();
     }
 
     @Override
